28f5db05df778cc5f842a03315c4cb6d45f487be
Juan Arenas <arenas.juan.f@gmail.com>
2023-04-02 12:50:01 -0700

    Updated code to reflect current working version

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ import base64
 import requests
 import config
 from config import SKU_REPLACEMENTS as SKU_REPLACEMENTS
+from concurrent.futures import ThreadPoolExecutor
 
 auth_string = f"{config.SHIPSTATION_API_KEY}:{config.SHIPSTATION_API_SECRET}"
 encoded_auth_string = base64.b64encode(auth_string.encode('utf-8')).decode('utf-8')
 
+
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Basic {encoded_auth_string}"
+}
+
+mlp_data = {}  # Define as a global variable
 
 def lambda_handler(event, context):
     response = {
@@ -14,18 +22,13 @@ def lambda_handler(event, context):
         "headers": {"Content-Type": "application/json"},
         "body": json.dumps({"message": "Webhook received and processed"}),
     }
-    
+
     payload = json.loads(event["body"])
 
     if "resource_url" in payload:
         resource_url = payload["resource_url"]
         print(f"Fetching data from resource_url: {resource_url}")
 
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Basic {encoded_auth_string}"
-        }
-        
         try:
             order_response = requests.get(resource_url, headers=headers)
         except requests.exceptions.RequestException as e:
@@ -35,13 +38,27 @@ def lambda_handler(event, context):
         else:
             if order_response.status_code == 200:
                 order_data = order_response.json()
+                print(order_data)
                 if "orders" in order_data:
-                    orders = order_data["orders"]
-                    print("Processing orders")
-                    for order in orders:
-                        process_order(order)
+                    order = order_data["orders"][0]
+                    print("Processing order")
 
-                    update_order(orders)  # Call the update_order function after processing all orders
+                    has_lawn_plan = any(isLawnPlan(item["sku"]) for item in order["items"])
+                    if has_lawn_plan:
+                        print("This order has a lawn plan")
+                        url_mlp = f"https://user-api-dev-qhw6i22s2q-uc.a.run.app/order?shopify_order_no={order['orderNumber']}"
+                        response_mlp = requests.get(url_mlp)
+                        data_mlp = response_mlp.json()
+                        plan_details = data_mlp.get("plan_details", [])
+                        for detail in plan_details:
+                            for product in detail['products']:
+                                mlp_data[detail['sku']] = {
+                                    'name': product['name'],
+                                    'count': product['count']
+                                }
+                        print(mlp_data[detail['sku']])
+                    process_items_and_update_order(order)
+                        
                 else:
                     print("No orders key found in order_data")
                     print(f"Order data: {order_data}")
@@ -56,7 +73,7 @@ def lambda_handler(event, context):
         print(f"Payload: {payload}")
         response["statusCode"] = 400
         response["body"] = json.dumps({"error": "No 'resource_url' key found in webhook payload"})
-    
+
     return response
 
 
@@ -65,81 +82,53 @@ def lambda_handler(event, context):
 def isLawnPlan(sku):
     return (sku.startswith('SUB') or sku in ['05000', '10000', '15000']) and sku not in ["SUB - LG - D", "SUB - LG - S", "SUB - LG - G"]
 
+def get_plan_details_from_url_mlp(order_number):
+    url_mlp = f"https://user-api-dev-qhw6i22s2q-uc.a.run.app/order?shopify_order_no={order_number}"
+    response_mlp = requests.get(url_mlp)
+    print("Sending request to:", url_mlp)
+    print("Response status code from MLP API:", response_mlp.status_code)
+    data_mlp = response_mlp.json()
+    plan_details = data_mlp.get("plan_details", [])
+    return plan_details
 
 
-def get_orders_by_order_number(order_number):
-    url = "https://ssapi.shipstation.com/orders"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Basic {encoded_auth_string}"
-    }
-    params = {
-        "orderNumber": order_number
-    }
-    response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 200:
-        orders = response.json()["orders"]
-        # Sort orders by the creation date in descending order
-        sorted_orders = sorted(orders, key=lambda x: x['createDate'], reverse=True)
-        return sorted_orders
-    else:
-        print(f"Error fetching orders: {response.content}")
-        return None
+def process_item(item):
+    global mlp_data 
+    print("Processing individual item")
+    original_sku = item["sku"]
+    if original_sku in SKU_REPLACEMENTS:
+        if isLawnPlan(original_sku) and original_sku in mlp_data:
+            product_info = mlp_data[original_sku]
+            item['name'] = SKU_REPLACEMENTS[original_sku] + f"\n\u00A0\u00A0\u00A0\u00A0• {product_info['count']} {product_info['name']}"
+        else:
+            replacement_name = SKU_REPLACEMENTS[original_sku]
+            item["name"] = replacement_name
+
+    print("Finished processing individual item")
+
+def process_items_and_update_order(order):
+    with ThreadPoolExecutor(max_workers=8) as executor:
+        # Process items concurrently
+        executor.map(process_item, order["items"])
+    update_order(order)
 
 
-
-def process_order(order):
-    print("Processing individual order")
-
-    for item in order["items"]:
-        original_sku = item["sku"]
-        if original_sku in SKU_REPLACEMENTS:
-            if isLawnPlan(original_sku):
-                order_number = order["orderNumber"]
-                url_mlp = f"https://user-api-dev-qhw6i22s2q-uc.a.run.app/order?shopify_order_no={order_number}"
-                response_mlp = requests.get(url_mlp)
-                print("Sending request to:", url_mlp)
-                print("Response status code from MLP API:", response_mlp.status_code)
-                data_mlp = response_mlp.json()
-                plan_details = data_mlp.get("plan_details", [])
-
-                item['name'] = SKU_REPLACEMENTS[original_sku]
-                
-                for plan in plan_details:
-                    products = plan.get("products", [])
-                    print("Products count:", len(products))
-                    print("Products:", products)
-                    
-                    # Iterate through products and append the formatted string
-                    for product in products:
-                        product_count = product.get("count", 0)
-                        product_name = product.get("name", "")
-                        item['name'] += f"\n\u00A0\u00A0\u00A0\u00A0• {product_count} {product_name}"
-
-            else:
-                replacement_name = SKU_REPLACEMENTS[original_sku]
-                item["name"] = replacement_name
-
-    print("Finished processing individual order")
-
-
-def update_order(orders):
-    print("Updating orders")
+def update_order(order):
+    print("Updating order")
     url = "https://ssapi.shipstation.com/orders/createorder"
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Basic {encoded_auth_string}"
     }
-    for order in orders:
-        print(json.dumps(order))
-        try:
-            response = requests.post(url, headers=headers, data=json.dumps(order))
-            print(f"Response status code: {response.status_code}")
-            print(f"Response content: {response.content}")
-        except requests.exceptions.RequestException as e:
-            print(f"Error updating order {order['orderNumber']} in Shipstation: {e}")
+    print(json.dumps(order))
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(order))
+        print(f"Response status code: {response.status_code}")
+        print(f"Response content: {response.content}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error updating order {order['orderNumber']} in Shipstation: {e}")
+    else:
+        if response.status_code != 200:
+            print(f"Failed to update order {order['orderNumber']} in Shipstation: {response.content}")
         else:
-            if response.status_code != 200:
-                print(f"Failed to update order {order['orderNumber']} in Shipstation: {response.content}")
-            else:
-                print(f"Successfully updated order {order['orderNumber']} in Shipstation")
+            print(f"Successfully updated order {order['orderNumber']} in Shipstation")
